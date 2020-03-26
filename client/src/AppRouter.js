01acee3366fb8b6ad0e18ad09e2be47e1418d263
spaceforestchu <spaceforestchu@gmail.com>
2020-03-26 03:49:42 -0400

    change cases for signin and signup

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -3,8 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import ToolBar from "./components/Toolbar/Toolbar";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
-const Signin = React.lazy(() => import("./components/Signin/SignIn"));
-const Signup = React.lazy(() => import("./components/Signup/SignUp"));
+const Signin = React.lazy(() => import("./components/SignIn/SignIn"));
+const Signup = React.lazy(() => import("./components/SignUp/SignUp"));
 const Landing = React.lazy(() =>
   import("./components/containers/Landing/Landing")
 );
